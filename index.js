bb71c2898e90d6cb074c0e6de8b179cb616bfd64
Ben <berkhart@rocketmail.com>
2022-08-10 11:02:01 -0500

    Completed Tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById('main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.className = 'victory';
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ben is the champion'
